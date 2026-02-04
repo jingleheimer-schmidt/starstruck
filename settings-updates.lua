fb4ad4042e2b530c3651190df63b1b2f7a86ffe2
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2026-02-03 22:59:34 -0800

    Set default paint status for cargo & fluid wagons
    Set the default_value of the Automatic Train Painter mod settings "paint-cargo-wagon" and "paint-fluid-wagon" to true in settings-updates.lua so wagons are painted by default. Also added a changelog entry in changelog.txt noting the change.

--- settings-updates.lua
+++ settings-updates.lua
@@ -2,3 +2,11 @@
 if data.raw["int-setting"]["ltlo-battery_drain"] then
     data.raw["int-setting"]["ltlo-battery_drain"].default_value = 0
 end
+
+if data.raw["bool-setting"]["paint-cargo-wagon"] then
+    data.raw["bool-setting"]["paint-cargo-wagon"].default_value = true
+end
+
+if data.raw["bool-setting"]["paint-fluid-wagon"] then
+    data.raw["bool-setting"]["paint-fluid-wagon"].default_value = true
+end
